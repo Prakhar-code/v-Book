695edea56e4b705f1bf28bfde1c9a3ea70195364
Prakhar Kabra <prakhar.rock09@gmail.com>
2025-02-25 01:20:46 +0530

    refactored admin routes for cabin and ticket management; implemented error handling and streamlined database interactions

--- backend/app/routes/admin_routes.py
+++ backend/app/routes/admin_routes.py
@@ -1,235 +1,132 @@
-import logging
-from fastapi.responses import JSONResponse
-from sqlalchemy import text
-from sqlalchemy.exc import SQLAlchemyError
-from app.core.db import engine
+from fastapi import APIRouter, HTTPException
+from app.models.cabin import Cabin
+from app.models.api_error import ErrorResponse
+from app.models.cabin_request import CabinRequest
 from app.models.ticket_update import TicketUpdate
-from app.services.email_service import send_ticket_mail
 from app.models.access_management_request import AccessManagementRequest
+from app.repositories.cabin_repo import (
+    add_cabin,
+    delete_cabin,
+    get_cabin,
+    get_cabin_request,
+    put_cabin_request,
+    get_all_cabin,
+    update_cabin,
+)
+from app.repositories.admin_repo import (
+    get_all_tickets,
+    get_ticket,
+    update_ticket,
+    provide_access,
+)
+
+app = APIRouter(prefix="/vbook/v1")
 
 
-def get_all_tickets():
+@app.get(
+    "/admins/cabins/requests",
+    responses={
+        400: {"model": ErrorResponse},
+        404: {"model": ErrorResponse},
+        500: {"model": ErrorResponse},
+    },
+)
+async def cabin_request():
+    return get_cabin_request()
+
+
+@app.put(
+    "/admins/cabins/requests",
+    responses={
+        400: {"model": ErrorResponse},
+        404: {"model": ErrorResponse},
+        500: {"model": ErrorResponse},
+    },
+)
+async def cabin_request(cabin_request: CabinRequest):
+    return put_cabin_request(cabin_request)
+
+
+@app.get("/admins/cabin-management/{cabin_id}")
+async def cabin_management(cabin_id):
+    return get_cabin(cabin_id)
+
+
+@app.get("/admins/cabin-management")
+async def cabin_management():
+    return get_all_cabin()
+
+
+@app.put("/admins/cabin-management")
+async def cabin_management(cabin: Cabin):
+    return update_cabin(cabin)
+
+
+@app.delete(
+    "/admins/cabin-management/{cabin_id}",
+    responses={
+        400: {"model": ErrorResponse},
+        404: {"model": ErrorResponse},
+        500: {"model": ErrorResponse},
+    },
+)
+async def cabin_management(cabin_id):
+    return delete_cabin(cabin_id)
+
+
+@app.get(
+    "/admins/manage-tickets",
+    responses={
+        400: {"model": ErrorResponse},
+        404: {"model": ErrorResponse},
+        500: {"model": ErrorResponse},
+    },
+)
+async def manage_tickets():
+    return get_all_tickets()
+
+
+@app.get(
+    "/admins/manage-tickets/{ticket_id}",
+    responses={
+        400: {"model": ErrorResponse},
+        404: {"model": ErrorResponse},
+        500: {"model": ErrorResponse},
+    },
+)
+async def get_ticket_by_id(ticket_id):
+    return get_ticket(ticket_id)
+
+
+@app.put(
+    "/admins/manage-tickets/{ticket_id}",
+    responses={
+        400: {"model": ErrorResponse},
+        404: {"model": ErrorResponse},
+        500: {"model": ErrorResponse},
+    },
+)
+async def manage_ticket(ticket_id: int, ticket: TicketUpdate):
+    return update_ticket(ticket_id, ticket)
+
+
+@app.put(
+    "/admins/access-management",
+    responses={
+        400: {"model": ErrorResponse},
+        404: {"model": ErrorResponse},
+        500: {"model": ErrorResponse},
+    },
+)
+async def access_management(request: AccessManagementRequest):
     try:
-        with engine.connect() as connection:
-            query = text(
-                """
-                SELECT t.*, u.name, u.email 
-                FROM Tickets t 
-                LEFT JOIN Users u ON t.user_id = u.user_id
-            """
-            )
-            result = connection.execute(query).fetchall()
-            if result:
-                result_list = []
-                for row in result:
-                    dict1 = {
-                        "ticket_id": row.ticket_id,
-                        "user_id": row.user_id,
-                        "cabin_id": row.cabin_id,
-                        "name": row.name,
-                        "email": row.email,
-                        "date": row.date.isoformat() if row.date else None,
-                        "description": row.description,
-                        "response": row.response,
-                        "status": row.status,
-                        "created_at": row.created_at.isoformat() + "Z",
-                    }
-                    result_list.append(dict1)
+        return provide_access(request)
 
-                return {"tickets": result_list, "status": "success"}
-            else:
-                return {"status": "success", "tickets": []}
-    except SQLAlchemyError as e:
-        logging.error(f"Database error while fetching tickets: {str(e)}")
-        return JSONResponse(
-            status_code=500,
-            content={
-                "status": "error",
-                "detail": "Database error occurred while fetching tickets",
-            },
-        )
+    except HTTPException as http_exc:
+        raise http_exc
+
     except Exception as e:
-        logging.error(f"Unexpected error while fetching tickets: {str(e)}")
-        return JSONResponse(
+        raise HTTPException(
             status_code=500,
-            content={"status": "error", "detail": "An unexpected error occurred"},
+            detail="An unexpected error occurred while sending the OTP. Please try again later.",
         )
-
-
-def get_ticket(ticket_id):
-    try:
-        with engine.connect() as connection:
-            query = text(
-                """
-                SELECT t.*, u.name, u.email, c.name as cabin_name 
-                FROM Tickets t 
-                LEFT JOIN Users u ON t.user_id = u.user_id 
-                LEFT JOIN Cabins c ON t.cabin_id = c.cabin_id
-                WHERE t.ticket_id = :ticket_id
-            """
-            )
-            result = connection.execute(query, {"ticket_id": ticket_id}).fetchone()
-
-            if result:
-                ticket_data = {
-                    "ticket_id": result.ticket_id,
-                    "user_id": result.user_id,
-                    "cabin_id": result.cabin_id,
-                    "cabin_name": result.cabin_name,
-                    "name": result.name,
-                    "email": result.email,
-                    "date": result.date.isoformat() if result.date else None,
-                    "description": result.description,
-                    "response": result.response,
-                    "status": result.status,
-                    "created_at": result.created_at.isoformat() + "Z",
-                }
-
-                return {"status": "success", "ticket": ticket_data}
-            else:
-                return JSONResponse(
-                    status_code=404,
-                    content={
-                        "status": "error",
-                        "detail": f"Ticket with ID {ticket_id} not found",
-                    },
-                )
-    except SQLAlchemyError as e:
-        logging.error(f"Database error while fetching ticket: {str(e)}")
-        return JSONResponse(
-            status_code=500,
-            content={
-                "status": "error",
-                "detail": "Database error occurred while fetching ticket details",
-            },
-        )
-    except Exception as e:
-        logging.error(f"Unexpected error while fetching ticket: {str(e)}")
-        return JSONResponse(
-            status_code=500,
-            content={"status": "error", "detail": "An unexpected error occurred"},
-        )
-
-
-def update_ticket(ticket_id: int, ticket: TicketUpdate):
-    try:
-        with engine.connect() as connection:
-            check_query = text(
-                """SELECT t.ticket_id, u.email, u.name
-                FROM Tickets t
-                JOIN Users u ON t.user_id = u.user_id
-                WHERE t.ticket_id = :ticket_id"""
-            )
-            exists = connection.execute(
-                check_query, {"ticket_id": ticket_id}
-            ).fetchone()
-
-            if not exists:
-                return JSONResponse(
-                    status_code=404,
-                    content={
-                        "status": "error",
-                        "detail": f"Ticket with ID {ticket_id} not found",
-                    },
-                )
-
-            # # Force status to CLOSED
-            # ticket.status = "CLOSED"
-
-            update_query = text(
-                """
-                UPDATE Tickets
-                SET status = :status,
-                    response = :response,
-                    description = :description,
-                    date = COALESCE(:date, date)
-                WHERE ticket_id = :ticket_id
-            """
-            )
-
-            connection.execute(
-                update_query,
-                {
-                    "ticket_id": ticket_id,
-                    "status": ticket.status,
-                    "response": ticket.response,
-                    "description": ticket.description,
-                    "date": ticket.date if ticket.date else None,
-                },
-            )
-            connection.commit()
-
-            # email, name, ticket_id, description, response, status, subject
-            print(exists.email)
-            print(exists.name)
-            print(ticket_id)
-            print(ticket.description)
-            print(ticket.response)
-            print(ticket.status)
-
-            send_ticket_mail(
-                exists.email,
-                exists.name,
-                ticket_id,
-                ticket.description,
-                ticket.response,
-                ticket.status,
-                subject="vBook - Update on your Ticket",
-            )
-
-            return {
-                "status": "success",
-                "message": "Ticket updated successfully",
-                "ticket_id": ticket_id,
-                "response": ticket.response,
-                "status": ticket.status,
-                "description": ticket.description,
-                "email": exists.email,
-                "name": exists.name,
-            }
-
-    except SQLAlchemyError as e:
-        logging.error(f"Database error while updating ticket: {str(e)}")
-        return JSONResponse(
-            status_code=500,
-            content={
-                "status": "error",
-                "detail": "Database error occurred while updating ticket",
-            },
-        )
-    except Exception as e:
-        logging.error(f"Unexpected error while updating ticket: {str(e)}")
-        return JSONResponse(
-            status_code=500,
-            content={"status": "error", "detail": str(e)},
-        )
-
-
-def provide_access(request: AccessManagementRequest):
-    try:
-
-        with engine.connect() as connection:
-            role = request.role
-            employee_id = request.employee_id
-            query = text(
-                "UPDATE Users SET role = :role WHERE employee_id = :employee_id"
-            )
-            result = connection.execute(query, {"role": role, "employee_id": employee_id})
-            connection.commit()
-
-            if result.rowcount > 0:
-                response_data = {
-                    "message": "Role Updated successfully",
-                    "status": "success",
-                }
-                return JSONResponse(content=response_data,status_code=200)
-            else:
-                response_data = {
-                    "message": "No user found",
-                    "status": "failed",
-                }
-                return JSONResponse(content=response_data, status_code=404)
-    except SQLAlchemyError as e:
-        logging.error(f"Database error while fetching cabin: {str(e)}")
-    except Exception as e:
-        logging.error(f"Unexpected error while fetching bookings: {str(e)}")
