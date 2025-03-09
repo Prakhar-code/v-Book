25614179d68ae0abeb95af21d81ab998de422998
Prakhar Kabra <prakhar.rock09@gmail.com>
2025-03-09 22:20:38 +0530

    add global styles, component-specific styles, and images for user and admin dashboards

---
+++ frontend/frontend-vbook/src/assets/styles/Colors.js
@@ -0,0 +1,13 @@
+const colors = {
+    navbar: '#010f6f',         
+    body: '#D1D9FF',      
+    buttonBackground: '#96D5FFFF',
+    buttonHover: '#00AAFFFF',     
+    success: '#2ecc71',         
+    warning: '#f39c12',
+    error: '#e74c3c',       
+    info: '#3498db',        
+  };
+  
+  export default colors;
+  
